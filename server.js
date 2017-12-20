f0f51f20dfc2472c8d9c0ed41e42fa42d0d17419
a-m-salman <ali._.salman91@hotmail.com>
2017-12-20 13:14:16 +0200

    new commit

--- server.js
+++ server.js
@@ -1,28 +1,75 @@
-var express = require('express');
+const express = require('express');
 const bodyParser= require('body-parser')
 const app = express()
+const assert= require('assert');
+// const mdb = require('mongodb');
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
-var assert= require('assert');
-const MongoClient = require('mongodb').MongoClient
 
-var url = "mongodb://localhost:27017/user";
+// const MongoClient = mdb.MongoClient
 
+// const url = "mongodb://localhost:27017/user";
 
-MongoClient.connect(url, (err, database) => {
-   if (err) return console.log(err);
-	  db = database;
-	  app.get('/', (req, res) => {
-				 db.db("user").collection('user').find().toArray(function(err, results) {
-				  
-    			res.render('index.ejs', {users: results})
-				})
+mongoose.connect('mongodb://localhost/user');
 
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
 
+// });
+
+
+
+//************************************* mongoosJS 
+var userSchema = mongoose.Schema({
+    name: String,
+    age: String,
+    gender: String
+
+},{collection: 'user'});
+
+var user = mongoose.model('user', userSchema);
+
+var x = new user({name: 'taha',age: '20',gender: 'm'});
+
+
+//  x.save(function (err, x) {
+//   if (err) return console.error(err);
+//   console.log("Done");
+// });
+
+user.find(function (err, user) {
+  if (err) return console.error(err);
+  console.log(user);
+
+  	  app.get('/', (req, res) => {
+    			res.render('index.ejs', {users: user})
 		})
-  });
+})
+
+
+
+
+//******************************************  MongoDB 
+
+
+// MongoClient.connect(url, (err, database) => {
+//    if (err) return console.log(err);
+// 	  db = database;
+// 	  app.get('/', (req, res) => {
+// 				 db.db("user").collection('user').find().toArray(function(err, results) {
+				  
+//     			res.render('index.ejs', {users: results})
+// 				})
+
+
+
+// 		})
+//   });
 
 var server = app.listen(8081, function () {
    var host = server.address().address
